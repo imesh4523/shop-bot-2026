04605fab23f5a883b8730a8dc38f83917e3f1e05
imesh4523 <imesh4523@gmail.com>
2026-09-04 12:47:32 +0530

    Auto delete Quick Menu text message after 5 seconds

--- server/routes.ts
+++ server/routes.ts
@@ -8526,10 +8526,13 @@ async function processAntiSpamCheck(userId: string, chatId: number, queryId?: st
               parse_mode: 'HTML',
               reply_markup: startInlineMarkup
             });
-            await targetBot.sendMessage(chatId, '<tg-emoji emoji-id="5938185976307258461">👇</tg-emoji> <b>Quick Menu</b>', {
+            const quickMenuMsg = await targetBot.sendMessage(chatId, '<tg-emoji emoji-id="5938185976307258461">👇</tg-emoji> <b>Quick Menu</b>', {
               parse_mode: 'HTML',
               reply_markup: bottomKeyboard
             });
+            setTimeout(() => {
+              targetBot.deleteMessage(chatId, quickMenuMsg.message_id).catch(() => {});
+            }, 5000);
             return;
           } catch (err: any) {
             console.error('Failed to send banner photo, falling back to text:', err.message);
